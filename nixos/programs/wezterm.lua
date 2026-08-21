42a9a9297fb971347bba0267e0c14f18dda41810
Matei Barbu <mateibarbu19@disroot.org>
2026-08-21 11:05:50 +0300

    wezterm: use the same padding, for now

--- nixos/programs/wezterm.lua
+++ nixos/programs/wezterm.lua
@@ -65,18 +65,13 @@ config.enable_scroll_bar = true
 -- Show tab bar for multiple tabs
 config.hide_tab_bar_if_only_one_tab = true
 
-no_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
 default_padding = {
   left = 5,
   right = 5,
   top = 5,
   bottom = 5,
 }
+no_padding = default_padding
 
 -- Disable scrollbar in alt screen aplications
 wezterm.on("update-status", function(window, pane)
